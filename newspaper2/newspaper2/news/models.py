4deed6e8636e00c7cd97f089ffe9a3410e4ca001
luisfer85 <luisfer85@gmail.com>
2017-09-22 16:46:49 +0200

    Clase 2: Creacion del modelo de eventos

--- newspaper2/newspaper2/news/models.py
+++ newspaper2/newspaper2/news/models.py
@@ -1,14 +1,28 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-class News(models.Model):
+
+class BaseNews(models.Model):
     title = models.CharField(_('title'), max_length=255)
     description = models.TextField(_('description'))
     publish_date = models.DateTimeField(_('publish_date'))
 
     class Meta:
-        verbose_name = _('news item')
-        verbose_name_plural = _('news')
+        abstract = True #Evita crear una tabla para esta clase de tal manera que estos campos se incluyen en las tablas heredadas
 
     def __str__(self):
         return self.title #Que queremos mostrar cuando listamos los elementos
+
+class News(BaseNews):
+
+    class Meta:
+        verbose_name = _('news item')
+        verbose_name_plural = _('news')
+
+class Event(BaseNews):
+    start_date = models.DateTimeField(_('start_date'))
+    end_date = models.DateTimeField(_('end_date'))
+
+    class Meta:
+        verbose_name = _('event')
+        verbose_name_plural = _('events')
