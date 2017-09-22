5c669d838615caa8e236f8283f6cddb949cda154
luisfer85 <luisfer85@gmail.com>
2017-09-22 16:22:33 +0200

    Clase 2: Creacion del modelo de noticias

--- newspaper2/newspaper2/news/models.py
+++ newspaper2/newspaper2/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class News(models.Model):
+    title = models.CharField(_('title'), max_length=255)
+    description = models.TextField(_('description'))
+    publish_date = models.DateTimeField(_('publish_date'))
+
+    class Meta:
+        verbose_name = _('news item')
+        verbose_name_plural = _('news')
+
+    def __str__(self):
+        return self.title #Que queremos mostrar cuando listamos los elementos
