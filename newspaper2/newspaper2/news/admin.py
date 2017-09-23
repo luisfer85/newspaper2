11afabd52379448f022db7b1b66713e669d6cf7b
luisfer85 <luisfer85@gmail.com>
2017-09-23 13:13:43 +0200

    Clase 3: Personalizamos el admin site

--- newspaper2/newspaper2/news/admin.py
+++ newspaper2/newspaper2/news/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from newspaper2.news.models import News, Event
 
 class NewsAdmin(admin.ModelAdmin):
-	pass
+    list_display = ('title', 'publish_date')
+    list_filter = ('publish_date',)
+    search_fields = ['title']
 
 class EventAdmin(admin.ModelAdmin):
 	pass
