8522bc29e5597113af9f9714e510548057e19315
luisfer85 <luisfer85@gmail.com>
2017-10-04 00:24:27 +0200

    Clase 7: Incluimos el atributo href al serializador que usa la API listado de noticias para conocer la url donde se encuentra

--- newspaper2/newspaper2/news/serializers.py
+++ newspaper2/newspaper2/news/serializers.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+
 from rest_framework import serializers
 
 from newspaper2.news.models import News
@@ -5,10 +7,15 @@ from newspaper2.news.models import News
 
 class NewsSerializer(serializers.ModelSerializer):
 
+    href = serializers.SerializerMethodField('get_href_url')  #Lo definimos como un metodo y lo llamamos get_href_url
 
     class Meta:
         model = News
-        fields = ('title', 'publish_date')
+        fields = ('title', 'publish_date', 'href')
+
+    def get_href_url(self, obj):
+        url = reverse('news_detail_api', args=(obj.pk,))  #Conseguimos la ruta relativa (ej: /api/news/8/)
+        return self.context.get('request').build_absolute_uri(url)  #Obtenemos el request de django y añadimos la ruta relativa
 
 
 class NewsSerializerComplete(serializers.ModelSerializer):
